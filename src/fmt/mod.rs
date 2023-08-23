0e58d1e4f72affb8862ee9f1401aa52f02f342f8
joseLuís <joseluis@andamira.net>
2023-08-24 00:00:36 +0200

    remove `int_buf` module
    - moved to `devela`.

--- src/fmt/mod.rs
+++ src/fmt/mod.rs
@@ -4,16 +4,10 @@
 //
 
 mod indent;
-#[cfg(not(feature = "safe"))]
-mod int_buf;
 
 pub use all::*;
 pub(super) mod all {
     #[doc(inline)]
     #[cfg(feature = "alloc")]
     pub use super::indent::indent;
-
-    #[doc(inline)]
-    #[cfg(not(feature = "safe"))]
-    pub use super::int_buf::{IntBuf, IntBufAble};
 }
