4611da1075c2f7e1bc9dfe80190ae095cbd55fca
joseLuís <joseluis@andamira.net>
2023-08-30 14:39:11 +0200

    fix no-std depreaction

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,23 @@
 //!
 //
 
+// warnings
 #![warn(clippy::all)]
 #![allow(clippy::wrong_self_convention)]
+// environment
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(feature = "safe", forbid(unsafe_code))]
 #![cfg_attr(feature = "nightly", feature(doc_cfg))]
-
-// features safeguarding
-#[cfg(all(feature = "std", feature = "no-std"))]
-compile_error!("You can't enable the `std` and `no-std` features at the same time.");
-#[cfg(all(feature = "safe", feature = "unsafe"))]
-compile_error!("You can't enable the `safe` and `unsafe` features at the same time.");
-
 #[cfg(feature = "alloc")]
 extern crate alloc;
+
+// safeguards
+#[cfg(all(feature = "std", feature = "no_std"))]
+compile_error!("You can't enable the `std` and `no_std` features at the same time.");
+#[cfg(all(feature = "safe", feature = "unsafe"))]
+compile_error!("You can't enable the `safe` and `unsafe*` features at the same time.");
+// deprecated
+deprecate_feature![old: "no-std", new: "no_std", since: "0.0.3"];
 
 pub mod error;
 
